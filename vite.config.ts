3975ba48757cb2cc2da7efd13b4fbacb09884165
FlorianTimm <florian.timm@hcu-hamburg.de>
2026-03-17 17:14:14 +0100

    PWA-Einstellungen von anderem Projekt übernommen

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 /// <reference types="vitest" />
 
 import vue from '@vitejs/plugin-vue'
-import legacy from '@vitejs/plugin-legacy'
+//import legacy from '@vitejs/plugin-legacy'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy(),
+    //legacy(),
+    VitePWA({ registerType: 'autoUpdate' }),
     basicSsl()
   ],
   resolve: {
